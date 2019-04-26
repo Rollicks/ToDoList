27f9b6ee5a05f6e0562308702ff4e9d2c064cb40
Rollicks Lakra <30549903+Rollicks@users.noreply.github.com>
2019-04-26 15:21:21 +0530

    Add files via upload

---
+++ Assets/JS/todo.js
@@ -0,0 +1,24 @@
+//clicking the li
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('completed');
+});
+
+$("ul").on('click', 'span', function(event) {
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$('input').keypress(function(event){
+    if(event.which === 13){
+        var txt = $(this).val();
+        console.log(txt);
+        $('ul').append('<li><span><i class="far fa-trash-alt"></i></span> '+ txt +'</li>');
+        $(this).val("");
+    }
+});
+
+$('.fa-plus').on('click', function(){
+    $('input').fadeToggle();
+});
